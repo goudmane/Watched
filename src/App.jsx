16e6f8bb61081554160e8f31e9eb9f35d87b7c04
khalil <khalilpati55@gmail.com>
2022-04-09 23:02:23 +0530

    Cleaned Project and Added Prettier Config

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Movies from './pages/Movies';
+
+function App() {
+  return (
+    <div className="container p-5">
+     <Movies/>
+    </div>
+  );
+}
+
+export default App;
