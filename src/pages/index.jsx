7e0915dac5e654d60b3064137df14f04b62fbd84
Khalil <khalilpati55@gmail.com>
2022-04-11 01:53:56 +0530

    Accumulating all the imports

---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as MovieDetail } from "./Movie";
+export { default as TopRated } from "./TopRated";
+export { default as TrendingMovies } from "./Trending";
+export { default as UpcomingMovies } from "./Upcoming";
