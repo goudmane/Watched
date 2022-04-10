8b886376423e44ed4cbc1211845f695ee71846ef
Khalil <khalilpati55@gmail.com>
2022-04-11 01:53:29 +0530

    Added API using axios and accessToken from env

---
+++ src/API/moviesService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+const baseUrl = `https://api.themoviedb.org/3/`;
+
+const API = axios.create({
+    baseURL: baseUrl,
+    headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+});
+export const getAllMovies = async () => {
+    const response = await API.get(`/discover/movie`);
+    const data = await response.data;
+    console.log(data);
+    return data;
+};
+
+export const getMoviesByEndPoint = async (endPoint, pageNum) => {
+    const response = await API.get(endPoint, {
+        params: {
+            page: pageNum,
+        },
+    });
+    const data = await response.data;
+    console.log(data);
+    return data;
+};
---
+++ src/hooks/moviesHooks.js
@@ -0,0 +1,2 @@
+const useMoviesAtHome = () => {};
+const useMoviesAtPage = () => {};
