f3f1285b1cb607353074513761d9f341fcdac757
Khalil <khalilpati55@gmail.com>
2022-04-12 02:33:55 +0530

    Fixed a Bug

--- src/API/moviesService.js
+++ src/API/moviesService.js
@@ -2,13 +2,10 @@ import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
-const baseUrl = `https://api.themoviedb.org/3/`;
-
+const baseUrl = `https://api.themoviedb.org/3`;
 const API = axios.create({
     baseURL: baseUrl,
-    headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-    },
+    headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
 });
 export const getAllMovies = async () => {
     const response = await API.get(`/discover/movie`);
@@ -24,6 +21,7 @@ export const getMoviesByEndPoint = async (endPoint, pageNum) => {
         },
     });
     const data = await response.data;
-    console.log(data);
     return data;
 };
+
+export const baseImgUrl = `https://image.tmdb.org/t/p/w200`;
