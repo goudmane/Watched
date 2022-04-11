f4f212dfa534b3983a347285f0c51f5408d3cb3f
Khalil <khalilpati55@gmail.com>
2022-04-12 02:33:35 +0530

    Added fetch hook to get movies by endpoint

--- src/hooks/moviesHooks.js
+++ src/hooks/moviesHooks.js
@@ -1,2 +1,19 @@
-const useMoviesAtHome = () => {};
-const useMoviesAtPage = () => {};
+import { useState, useEffect } from "react";
+import { getMoviesByEndPoint } from "../API/moviesService";
+
+export const useFetchByMoviesEndpoint = (endPoint, pageNum) => {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+            const { results } = await getMoviesByEndPoint(endPoint, pageNum);
+            setMovies(results);
+            setLoading(false);
+        })();
+    }, [endPoint, pageNum]);
+    return { movies, loading };
+};
+
+const useFetchByMovieID = () => {};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import MovieCard from "../../components/MovieCard";
+import { useFetchByMoviesEndpoint } from "../../hooks/moviesHooks";
 
 const Home = () => {
-    return <div>Home</div>;
+    const { movies: trendingMovies, loading } = useFetchByMoviesEndpoint(
+        "/trending/movie/week"
+    );
+    const { movies: upcomingMovies } =
+        useFetchByMoviesEndpoint("/movie/upcoming");
+    const { movies: topRatedMovies } =
+        useFetchByMoviesEndpoint("/movie/top_rated");
+
+    console.log(loading);
+    console.log("[TRENDING MOVIES]", trendingMovies);
+    console.log("[UPCOMING MOVIES]", upcomingMovies);
+    console.log("[TOPRATED MOVIES]", topRatedMovies);
+
+    return (
+        <div className="movie-grid">
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+        </div>
+    );
 };
 
 export default Home;
