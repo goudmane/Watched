4f88d80d739d96cf3f5a47480612f8473bc0dcff
Khalil <khalilpati55@gmail.com>
2022-04-14 01:48:50 +0530

    Defined services to get various data using movieId

--- src/API/moviesService.js
+++ src/API/moviesService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { createLinkFromData } from "../utils/helperFunctions";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
-const baseUrl = `https://api.themoviedb.org/3`;
+const baseUrl = `https://api.themoviedb.org/3/`;
 const API = axios.create({
     baseURL: baseUrl,
     headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
@@ -24,4 +25,28 @@ export const getMoviesByEndPoint = async (endPoint, pageNum) => {
     return data;
 };
 
+export const getMovieById = async (movieId) => {
+    const response = await API.get(`movie/${movieId}`);
+    const data = await response.data;
+    console.log(data);
+    return data;
+};
+
+export const getCreditsByMovieId = async (movieId) => {
+    const response = await API.get(`movie/${movieId}/credits`);
+    const { crew, cast } = await response.data;
+    return { crew, cast };
+};
+
+export const getRecommendationsByMovieId = async (movieId) => {
+    const response = await API.get(`movie/${movieId}/recommendations`);
+    const data = await response.data;
+    return data ? data.results.slice(0, 8) : [];
+};
+export const getVideoSourceByMovieId = async (movieId) => {
+    const response = await API.get(`movie/${movieId}/videos`);
+    const data = await response.data;
+    const link = createLinkFromData(data);
+    return link;
+};
 export const baseImgUrl = `https://image.tmdb.org/t/p`;
