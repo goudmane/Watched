29544e71b343041954a73831a230a5956a461519
Khalil <khalilpati55@gmail.com>
2022-04-14 01:48:18 +0530

    Added Helper Functions to aid movieData

---
+++ src/utils/helperFunctions.js
@@ -0,0 +1,29 @@
+import { baseImgUrl } from "../API/moviesService";
+
+export const createLinkFromData = (data) => {
+    const key = data.results.filter(
+        (video) =>
+            video.type === "Trailer" && video.site === "YouTube" && video.key
+    )[0].key;
+    const link = `https://www.youtube.com/watch?v=${key}`;
+
+    return link;
+};
+
+export const getDirectorFromCrew = (crew) => {
+    return crew.filter((person) => person.job === "Director");
+};
+
+export const getWritersFromCrew = (crew) => {
+    return crew.filter(
+        (person) => person.job === "Writer" || person.job === "Novel"
+    );
+};
+
+export const rectifyImageLinks = (data, width) => {
+    if (typeof data === "string") {
+        return `${baseImgUrl}/${width}${data}`;
+    } else {
+        return data.map((d) => `${baseImgUrl}/${width}${d}`);
+    }
+};
