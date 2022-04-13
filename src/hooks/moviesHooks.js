bd1bf42d61ed04cdd3b35dd34e375abda4bd3a23
Khalil <khalilpati55@gmail.com>
2022-04-14 01:49:14 +0530

    Built a Hook that fetch movie data using movieID

--- src/hooks/moviesHooks.js
+++ src/hooks/moviesHooks.js
@@ -1,5 +1,16 @@
 import { useState, useEffect } from "react";
-import { getMoviesByEndPoint } from "../API/moviesService";
+import {
+    getCreditsByMovieId,
+    getMovieById,
+    getMoviesByEndPoint,
+    getRecommendationsByMovieId,
+    getVideoSourceByMovieId,
+} from "../API/moviesService";
+import {
+    getDirectorFromCrew,
+    getWritersFromCrew,
+    rectifyImageLinks,
+} from "../utils/helperFunctions";
 
 export const useFetchByMoviesEndpoint = (endPoint, pageNum) => {
     const [movies, setMovies] = useState([]);
@@ -16,4 +27,40 @@ export const useFetchByMoviesEndpoint = (endPoint, pageNum) => {
     return { movies, loading };
 };
 
-const useFetchByMovieID = () => {};
+export const useFetchByMovieID = (movieId) => {
+    const [movie, setMovie] = useState({});
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+            const movieData = await getMovieById(movieId);
+            const { crew, cast } = await getCreditsByMovieId(movieId);
+            const recommendedData = await getRecommendationsByMovieId(movieId);
+            const youtubeLink = await getVideoSourceByMovieId(movieId);
+            setMovie(() => ({
+                ...movieData,
+                poster_path: rectifyImageLinks(movieData.poster_path, "w300"),
+                backdrop_path: rectifyImageLinks(
+                    movieData.backdrop_path,
+                    "original"
+                ),
+                youtube_link: youtubeLink,
+                cast: cast
+                    .slice(0, 10)
+                    .map((c) => ({
+                        ...c,
+                        profile_path: rectifyImageLinks(
+                            c.profile_path,
+                            "original"
+                        ),
+                    })),
+                director: getDirectorFromCrew(crew),
+                writers: getWritersFromCrew(crew),
+                recommended: recommendedData,
+            }));
+
+            setLoading(false);
+        })();
+    }, [movieId]);
+    return { movie, loading };
+};
