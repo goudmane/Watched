03bfb6ea6d61cf6c617408270cbfd418aee24e73
Khalil <khalilpati55@gmail.com>
2022-04-19 00:49:30 +0530

    Added Movie detail and its corresponding styles

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { FaStar } from "react-icons/fa";
+import { FaGreaterThan, FaStar } from "react-icons/fa";
 import Banner from "../../components/Banner";
 import { useFetchByMovieID } from "../../hooks/moviesHooks";
 import { movie } from "../../utils/links";
@@ -27,10 +27,85 @@ const MovieDetail = () => {
                                 .map((genre) => genre.name)
                                 .join(" / ")}
                         </div>
-
-                        <div className="movie-rating">
-                            <FaStar className="rating-star" />
-                            <span>{movie.vote_average} / 10</span>
+                        <div className="movie-stats">
+                            <div className="movie-rating">
+                                <FaStar className="rating-star" />
+                                <span>{movie.vote_average} / 10</span>
+                            </div>
+                            <div className="movie-numbers">
+                                <div className="movie-runtime">
+                                    <div className="runtime-label">
+                                        Running Time
+                                    </div>
+                                    <div className="runtime-value">
+                                        {movie.runtime} mins
+                                    </div>
+                                </div>
+                                <FaGreaterThan />
+                                <div className="release-date">
+                                    <div className="release-label">
+                                        Release Date
+                                    </div>
+                                    <div className="release-value">
+                                        {movie.release_date}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr style={{ margin: "1rem 0" }} />
+                    <div className="movie-display">
+                        <div className="movie-poster">
+                            <div className="poster">
+                                <img
+                                    src={movie.poster_path}
+                                    alt={movie.title}
+                                />
+                            </div>
+                            <div className="info">
+                                <div className="box">
+                                    <div className="label">Director</div>
+                                    <div className="content">
+                                        {movie.director
+                                            .map((c) => c.name)
+                                            .join(", ")}
+                                    </div>
+                                </div>
+                                <div className="box">
+                                    <div className="label">Writers</div>
+                                    <div className="content">
+                                        {movie.writers
+                                            .map((c) => c.name)
+                                            .join(", ")}
+                                    </div>
+                                </div>
+                                <div className="box">
+                                    <div className="label">Cast</div>
+                                    <div className="content">
+                                        {movie.cast
+                                            .slice(0, 4)
+                                            .map((c) => c.name)
+                                            .join(", ")}
+                                    </div>
+                                </div>
+                                <hr />
+                                <div className="box">
+                                    <div className="label">Plot</div>
+                                    <div className="content">
+                                        {movie.overview}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="movie-trailer">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src={movie.youtube_link}
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
