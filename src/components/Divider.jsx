7c8eb371077b97bb02ae9afec0b21a5a283cde70
Khalil <khalilpati55@gmail.com>
2022-05-11 18:46:18 +0530

    Add horizontal divider

---
+++ src/components/Divider.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Divider = () => {
+    return <hr style={{ margin: "1rem 0" }} />;
+};
+
+export default Divider;
--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Banner from "../../components/Banner";
 import { useFetchByMovieID } from "../../hooks/moviesHooks";
-// import { movie } from "../../utils/links";
 import MovieInfo from "../../components/MovieInfo";
 import MovieDisplay from "../../components/MovieDisplay";
+import Divider from "../../components/Divider";
 
 const MovieDetail = () => {
     const { movieId } = useParams();
@@ -12,12 +12,13 @@ const MovieDetail = () => {
     if (loading) return <div>Loading...</div>;
     return (
         <>
-            <Banner page="detail" />
+            <Banner page="detail" movie={movie} />
             <div className="container">
                 <div className="movie-details">
                     <MovieInfo movie={movie} />
-                    <hr style={{ margin: "1rem 0" }} />
+                    <Divider />
                     <MovieDisplay movie={movie} />
+                    <Divider />
                 </div>
             </div>
         </>
