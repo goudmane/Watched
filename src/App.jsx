5f604fa0d21d1f7494ae596989dfa5066e28a734
Khalil <khalilpati55@gmail.com>
2022-05-14 18:31:37 +0530

    Added Scroll to top component to get to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Header } from "./components";
+import { Header, ScrollToTop } from "./components";
 import {
     Home,
     TrendingMovies,
@@ -11,7 +11,8 @@ import {
 function App() {
     return (
         <>
-            <Header/>
+            <ScrollToTop />
+            <Header />
             <Routes>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/top-rated" element={<TopRated />} />
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 export { default as Banner } from "./Banner";
+export { default as ScrollToTop } from "./ScrollToTop";
 export { default as Divider } from "./Divider";
 export { default as Loader } from "./Loader";
 export { default as Header } from "./Header";
