c9162f64ca6b53f4a9284e99553284fa23928fb5
Khalil <khalilpati55@gmail.com>
2022-05-15 23:58:38 +0530

    Added movies to trending page using custom hook

--- src/pages/Trending/index.jsx
+++ src/pages/Trending/index.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { Loader, MoviesList } from "../../components";
+import { useFecthAtCategoryPage } from "../../hooks/moviesHooks";
+import { endPoints } from "../../utils/endpoints";
 
 const TrendingMovies = () => {
+    const { movies, loading, error } = useFecthAtCategoryPage(
+        endPoints.trendingMovies
+    );
+    if (loading) return <Loader />;
+    if (error) return <h2>{error}</h2>;
     return (
         <>
             <Helmet>
                 <title>Movify - Trending Movies</title>
             </Helmet>
             <section>
-                <div className="container">Trending Movies</div>
+                <div className="container">
+                    <h1>Trending Movies</h1>
+                    <div>
+                        <MoviesList movies={movies} />
+                    </div>
+                </div>
             </section>
         </>
     );
