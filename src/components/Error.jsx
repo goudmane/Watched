a81b8f3d62dbdfdec2f19d904a42b3d8412ecf39
Khalil <khalilpati55@gmail.com>
2022-05-18 22:52:41 +0530

    Add component to display error in a better way

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Error = () => {
+const Error = ({ error }) => {
+    const errorMsg =
+        error.status === 404 ? "Page not Found" : "Oops, something went wrong";
+    const errorFix = error.status === 404;
     return (
-        <div className="error-card">
-            <div className="error-status">
-                <span className="error-code">500</span> Server Error
-            </div>
-            <div className="error-message">Oops, something went wrong</div>
-            <div className="error-fix">
-                Try to refresh this page or feel free to contact us if the
-                problem persists
+        <div className="error-container">
+            <div className="error-card">
+                <div className="error-status">{error.status}</div>
+                <div className="error-message">{errorMsg}</div>
+                <div className="error-fix">
+                    {errorFix ? (
+                        <Link to="/"> Go Back </Link>
+                    ) : (
+                        " Try to refresh this page or feel free to contact us if the problem persists"
+                    )}
+                </div>
             </div>
         </div>
     );
